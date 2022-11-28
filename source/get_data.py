93a95ec0827b07441d21813c30d0711c95ff7b53
Joseph Maa <josephgmaa@berkeley.edu>
2022-11-28 13:05:36 -0800

    Update files

--- source/get_data.py
+++ source/get_data.py
@@ -9,7 +9,7 @@ def main():
     os.makedirs(f"data/{DOWNLOAD_REPO}", exist_ok=True)
     for i in range(3):
         with open(f"data/{DOWNLOAD_REPO}/pull_requests_{i}.json", "w") as pull_request_json:
-            subprocess.run(["gh", "api" , "-H", "Accept: application/vnd.github+json", "-H", f"Authorization: Bearer {API_KEY}", DOWNLOAD_REPO], stdout=pull_request_json)
+            subprocess.run(["gh", "api" , "-H", "Accept: application/vnd.github+json", "-H", f"Authorization: Bearer {API_KEY}", DOWNLOAD_REPO+f"&page={i+1}"], stdout=pull_request_json)
 
 
 if __name__ == "__main__":
